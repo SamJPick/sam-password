6e2d86f7a9c1df12118d44eb88b590dd9ab73a6d
Sam Pick <163494413+SamJPick@users.noreply.github.com>
2024-04-22 17:14:51 -0500

    Fixed chmod values in setup script

--- sam-password-setup.sh
+++ sam-password-setup.sh
@@ -9,6 +9,6 @@ fi
 cp -r `find /home/ -type d -name sam-password-main` /tmp/sam-password
 mv /tmp/sam-password/sam-password.sh /bin/sam-password
 gcc -o /bin/sam-password-encrypter /tmp/sam-password/sam-password-encrypter.c
-chmod 777 /bin/sam-password
-chmod 777 /bin/sam-password-encrypter
+chmod 755 /bin/sam-password
+chmod 755 /bin/sam-password-encrypter
 rm -r /tmp/sam-password
